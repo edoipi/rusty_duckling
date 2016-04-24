6c8cbefee39f2c8dfa6f348cdefda8011b98af1a
Mateusz Twaróg <mail@mateusztwarog.com>
2016-04-24 19:41:55 +0200

    Unit propagation - not really well tested, we shall see

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 #[macro_use] extern crate text_io;
 extern crate regex;
 use regex::Regex;
-use std::collections::BTreeMap;
-use std::collections::BTreeSet;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::linked_list::LinkedList;
 use std::io;
+use std::collections::hash_set::Iter;
 
 struct Clause {
-    literals: BTreeSet<i32>,
+    literals: HashSet<i32>,
 }
 
 struct Instance {
     nbvar: i32,
     nbclauses: i32,
-    clauses: BTreeMap<i32, Clause>
+    assignment: Vec<bool>,
+    literals: HashSet<usize>,
+    clauses: HashMap<i32, Clause>
 }
 
 fn id(x: bool) -> bool {
@@ -27,12 +31,12 @@ fn lit_num(var: usize, pos: bool) -> i32 {
     if pos {var as i32} else {-(var as i32)}
 }
 
-fn check(instance: &Instance, vec: &Vec<bool>) -> bool {
+fn check(instance: &Instance) -> bool {
     for (_, ref clause) in &instance.clauses {
         let mut satisfied = false;
         for &literal in &clause.literals {
             let fun: fn(bool) -> bool = if literal > 0 {id} else {not};
-            let val = vec[(literal.abs() - 1) as usize];
+            let val = instance.assignment[(literal.abs() - 1) as usize];
             satisfied = fun(val);
             if satisfied {
                 break;
@@ -45,29 +49,31 @@ fn check(instance: &Instance, vec: &Vec<bool>) -> bool {
     return true;
 }
 
-fn subsets(instance: &Instance, vec: &mut Vec<bool>, depth: usize) -> bool {
-    if depth == vec.len() {
-        return check(instance, vec);
-    }
-    vec[depth] = false;
-    if subsets(instance, vec, depth + 1) {
-        return true;
-    }
-    vec[depth] = true;
-    if subsets(instance, vec, depth + 1) {
-        return true;
-    }
-    return false;
+fn subsets(instance: &mut Instance, iter: &mut Iter<usize>) -> bool {
+    match iter.next() {
+        None => { 
+            return check(instance)
+        },
+        Some(&depth) => {
+            instance.assignment[depth] = false;
+            if subsets(instance, iter) {
+                return true;
+            }
+            instance.assignment[depth] = true;
+            if subsets(instance, iter) {
+                return true;
+            }
+            return false;
+        }
+    };
 }
 
-fn simple_solve(instance: Instance) {
-    let mut vec = vec![false; instance.nbvar as usize];
-    let satisfiable = subsets(&instance, &mut vec, 0);
+fn print_solution(satisfiable: bool, instance: &Instance) {
     if satisfiable {
         println!("s SATISFIABLE");
         print!("v ");
-        for (i, val) in vec.iter().enumerate() {
-            print!("{} ", lit_num(i + 1, *val));
+        for (i, &val) in instance.assignment.iter().enumerate() {
+            print!("{} ", lit_num(i + 1, val));
         }
         println!("0");
     } else {
@@ -75,8 +81,60 @@ fn simple_solve(instance: Instance) {
     }
 }
 
-fn unit_propagation(instance: Instance) {
-    //TO DO
+fn simple_solve(mut instance: &mut Instance) {
+    let not_set = instance.literals.clone(); //no idea how to fix that
+
+    let satisfiable = subsets(&mut instance, &mut not_set.iter());
+    
+    print_solution(satisfiable, instance);
+}
+
+fn unit_propagation(instance: &mut Instance) -> bool {
+    let mut queue = LinkedList::new();
+    let mut literal_map : HashMap<i32, Vec<i32>> = HashMap::new(); 
+    for (&clause_id, ref clause) in &instance.clauses {
+        if clause.literals.len() == 1 {
+            queue.push_back(clause.literals.iter().next().unwrap().clone());
+        }
+        for &literal in &clause.literals {
+            literal_map.entry(literal.abs()).or_insert(Vec::new()).push(clause_id);
+        }
+    }
+    while !queue.is_empty() {
+        let literal = queue.pop_front().unwrap();
+        for &clause_id in &literal_map[&literal.abs()] {
+            {
+                let clause = unsafe {&mut *(
+                    match instance.clauses.get_mut(&clause_id) {
+                        Some(val) => val,
+                        None => continue,
+                    } as *mut Clause)}; //don't ask
+                
+                if clause.literals.remove(&literal) {
+                    instance.clauses.remove(&clause_id);
+                }
+                
+                if clause.literals.remove(&(-literal)) && clause.literals.len() == 0 {
+                    return false;
+                }
+                
+                if clause.literals.len() == 1 {
+                    queue.push_back(clause.literals.iter().next().unwrap().clone());
+                }
+                
+                instance.assignment[(literal.abs() - 1) as usize] = if literal > 0 {true} else {false};
+            }
+        }
+    }
+    
+    instance.literals.clear();
+    for (_, ref clause) in &mut instance.clauses {
+        for &literal in &clause.literals {
+            instance.literals.insert(literal.abs() as usize);
+        }
+    }
+    
+    return true;
 }
 
 fn main() {
@@ -94,7 +152,7 @@ fn main() {
         };
     }
     
-    let mut instance = Instance {nbvar: 0, nbclauses: 0, clauses: BTreeMap::new()};
+    let mut instance = Instance {nbvar: 0, nbclauses: 0, assignment: Vec::new(), literals: HashSet::new(), clauses: HashMap::new()};
     let re = Regex::new("p cnf.*?([0-9]+).*?([0-9]+)").unwrap();
     match re.captures(&input) {
         Some(caps) => {
@@ -105,7 +163,7 @@ fn main() {
     };
     
     for i in 0..instance.nbclauses {
-        instance.clauses.insert(i, Clause{literals: BTreeSet::new()});
+        instance.clauses.insert(i, Clause{literals: HashSet::new()});
         loop {
             let tmp: i32 = read!();
             match tmp {
@@ -115,6 +173,12 @@ fn main() {
         }
     }
     
-    simple_solve(instance);
+    instance.assignment = vec![false; instance.nbvar as usize];
+    instance.literals = (0..instance.nbvar as usize).collect();
+    if unit_propagation(&mut instance) {
+        simple_solve(&mut instance);
+    } else {
+        print_solution(false, &instance);
+    }
 }
 
