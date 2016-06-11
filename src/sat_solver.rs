2e6bbfe21f3f2f4848d4a22e4126b7c3c9671bad
patryku <patryk.u93@gmail.com>
2016-06-11 15:21:35 +0200

    Luby implementation

--- src/sat_solver.rs
+++ src/sat_solver.rs
@@ -6,6 +6,24 @@ pub struct Luby {
 	pub k : i32
 }
 
+impl Luby {
+    pub fn new() -> Luby {
+    	Luby {seq : Vec::new(), index : 0, k : 1}
+    }
+
+    pub fn next(& mut self) -> i32 {
+    	self.index += 1;
+    	if self.index == (1 << self.k) - 1 {
+    		self.seq.push(1 << (self.k - 1));
+    		self.k += 1;
+    	} else {
+    		let val = self.seq[(self.index - (1 << (self.k - 1))) as usize];
+    		self.seq.push(val);
+    	}
+    	self.seq.last().unwrap().clone()
+    }
+}
+
 pub struct SatSolver {
 	pub cnf_manager : CnfManager,
 	pub luby : Luby,
