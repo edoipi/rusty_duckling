06ecc405a954aec009ff9a043c8d3b86d28cfb28
patryku <patryk.u93@gmail.com>
2016-06-13 17:56:15 +0200

    Main loop implemented

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,21 @@ use rusty_duckling::*;
 use std::io;
 
 fn main() {
-    let mut input = String::new();
-    let _ = io::stdin().read_line(&mut input);
-    
-    let instances_count = input.trim().parse::<i32>().unwrap();
-    
-    for _ in 0..instances_count {
-        let mut instance = preprocess();
-        /*println!("\n{} {} {}\n", instance.var_count, instance.clause_count, instance.clause_sum_length);
-        for vec in instance.clauses {
-            for val in vec {
-                print!("{} ", val);
-            }
-            println!("");
-        }*/
-    }
+	let mut input = String::new();
+	let _ = io::stdin().read_line(&mut input);
+
+	let instances_count = input.trim().parse::<i32>().unwrap();
+
+	for _ in 0..instances_count {
+		let ref instance = preprocess();
+		let mut sat_solver = SatSolver::new(instance);
+		let satisfiable = sat_solver.run();
+		if satisfiable {
+			println!("SAT");
+			sat_solver.printSolution();
+		} else {
+			println!("UNSAT");
+		}
+	}
 }
 
