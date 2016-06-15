b98219994e33fbbdb1e0b4f7393cda306f718569
butozerca <Michal.Dyrek@sabre.com>
2016-06-15 17:17:43 +0200

    formatted

--- cpp/Cnf.h
+++ cpp/Cnf.h
@@ -24,13 +24,15 @@ Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef _CNF
 #define _CNF
 
-struct Cnf{
-	unsigned vc;	// var count
-	unsigned cc;	// clause count
-	int **clauses;	// 2-dim. array with entries same as in cnf file
-	unsigned lc;	// literal count
-	unsigned *cl;	// clause length
-	Cnf(char *fname);
-	~Cnf();
+struct Cnf {
+    unsigned vc;    // var count
+    unsigned cc;    // clause count
+    int **clauses;    // 2-dim. array with entries same as in cnf file
+    unsigned lc;    // literal count
+    unsigned *cl;    // clause length
+    Cnf(char *fname);
+
+    ~Cnf();
 };
+
 #endif
