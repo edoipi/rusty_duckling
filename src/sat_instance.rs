bd6e62fa0a279c0d4b06c5f1b52a766243869999
patryku <patryk.u93@gmail.com>
2016-06-15 20:32:43 +0200

    Removed unused field

--- src/sat_instance.rs
+++ src/sat_instance.rs
@@ -3,7 +3,6 @@ use std::io;
 pub struct SatInstance {
 	pub var_count : i32,
 	pub clause_count : i32,
-	pub clause_sum_length : i32,
 	pub clauses : Vec<Vec<i32>>
 }
 
@@ -11,7 +10,7 @@ impl SatInstance {
 	pub fn read() -> SatInstance {
 		let mut input = String::new();
 		let _ = io::stdin().read_line(&mut input);
-		let mut instance = SatInstance {var_count: 0, clause_count: 0, clause_sum_length: 0, clauses: Vec::new()};
+		let mut instance = SatInstance {var_count: 0, clause_count: 0, clauses: Vec::new()};
 		{
 			let mut iter = input.split_whitespace();
 			iter.next();
@@ -30,7 +29,6 @@ impl SatInstance {
 							vec.push(it.parse::<i32>().unwrap());
 						}
 					}
-					instance.clause_sum_length += vec.len() as i32;
 					instance.clauses.push(vec);
 				},
 				Err(error) => println!("error: {}", error),
